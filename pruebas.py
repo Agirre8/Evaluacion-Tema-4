620bc267c17550b8ae8a063aeb0abcd197c11965
Agirre8 <inagi30@gmail.com>
2022-11-24 12:25:18 +0100

    clase nodo del arbol

---
+++ pruebas.py
@@ -0,0 +1,12 @@
+
+import heapq
+
+class node:
+    def __init__(self, freq, symbol, left=None, right=None):
+        #
+        self.freq = freq
+        self.symbol = symbol
+        self.left = left
+        self.right = right
+
+
