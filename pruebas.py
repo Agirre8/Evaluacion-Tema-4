ae48413ad03aa34e7e3f26f8809938800677fdf2
Agirre8 <inagi30@gmail.com>
2022-11-24 12:46:46 +0100

    metodo para imprimir nodo

--- pruebas.py
+++ pruebas.py
@@ -5,15 +5,34 @@
 #menor o igual que cualquiera de sus hijos. Esta libreria utiliza arrays.
 import heapq
 
-class node:
-    def __init__(self, freq, symbol, left=None, right=None):
+class nodo:
+    def __init__(self, freq, simbolo, left=None, right=None):
         
         self.freq = freq
-        self.symbol = symbol
+        self.symbol = simbolo
         self.left = left
         self.right = right
     
     def __lt__(self, nxt): #metodo less than, es para comprobar numeros menores que.
         return self.freq < nxt.freq
 
+def printNodo(nodo, val=""):
 
+    #codigo uffman para el nodo actual
+    newVal = val + str(nodo.huff)
+
+    #si el nodo no es un nodo de esquina
+    if(nodo.left):
+        printNodo(nodo.left, newVal)
+    if(nodo.left):
+        printNodo(nodo.right, newVal)
+
+    #si el nodo es un nodo de esquina
+
+    if(not nodo.left and not nodo.right):
+        print(f"{nodo.simbolo} -->{newVal}")
+
+
+simbolo = ["A", "F", "1", "3", "0", "M", "T"]
+
+freq = []
