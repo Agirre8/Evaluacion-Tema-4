50d27b04463531d557eba5b53731d6a51097a684
Agirre8 <inagi30@gmail.com>
2022-11-25 11:59:24 +0100

    clase arbol

--- Ejercicio2/arbolPokemon.py
+++ Ejercicio2/arbolPokemon.py
@@ -1,4 +1,4 @@
-class nodo:
+class Nodo:
     def __init__(self, dato):
         self.dato=dato
         self.izquierda=None
@@ -7,3 +7,14 @@ class Arbol:
     # Funciones privadas
     def __init__(self, dato):
         self.raiz = Nodo(dato)
+def __agregar_recursivo(self, nodo, dato):
+        if dato < nodo.dato:
+            if nodo.izquierda is None:
+                nodo.izquierda = Nodo(dato)
+            else:
+                self.__agregar_recursivo(nodo.izquierda, dato)
+        else:
+            if nodo.derecha is None:
+                nodo.derecha = Nodo(dato)
+            else:
+                self.__agregar_recursivo(nodo.derecha, dato)
