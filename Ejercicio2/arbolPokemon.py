d9117be220db04fa498439d656a2a1bd455fe4aa
Agirre8 <inagi30@gmail.com>
2022-11-25 12:02:17 +0100

    buscar

--- Ejercicio2/arbolPokemon.py
+++ Ejercicio2/arbolPokemon.py
@@ -33,3 +33,12 @@ class Arbol:
             self.__postorden_recursivo(nodo.izquierda)
             self.__postorden_recursivo(nodo.derecha)
             print(nodo.dato, end=", ")
+    def __buscar(self, nodo, busqueda):
+        if nodo is None:
+            return None
+        if nodo.dato == busqueda:
+            return nodo
+        if busqueda < nodo.dato:
+            return self.__buscar(nodo.izquierda, busqueda)
+        else:
+            return self.__buscar(nodo.derecha, busqueda)
