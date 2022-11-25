5725d387e0670e8a0180e7b01f7dbd1738d4d611
Agirre8 <inagi30@gmail.com>
2022-11-25 11:58:28 +0100

    clase arbol

--- Ejercicio2/arbolPokemon.py
+++ Ejercicio2/arbolPokemon.py
@@ -3,3 +3,7 @@ class nodo:
         self.dato=dato
         self.izquierda=None
         self.derecha=None
+class Arbol:
+    # Funciones privadas
+    def __init__(self, dato):
+        self.raiz = Nodo(dato)
